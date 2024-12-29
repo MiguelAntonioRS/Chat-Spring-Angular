2886817f454b6bac24e4977189b1dd5066eaa079
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2024-12-29 16:37:08 -0500

    first commit

---
+++ Backend-Spring/src/main/java/com/chat/chatSocket/dto/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.chat.chatSocket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ChatMessage {
+
+    private String message;
+    private String user;
+}
